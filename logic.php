10fae180354667582ae7a4b6cba157a3af25d3a7
ebo-one <e@ebo.one>
2016-02-18 00:31:52 -0500

    Add array of words

---
+++ logic.php
@@ -0,0 +1,33 @@
+<?php
+
+#Create array of words to generate password from
+$words = [
+    "01" => "aardvark",
+    "02" => "banana",
+    "03" => "champion",
+    "04" => "didgeridoo",
+    "05" => "equestrian",
+    "06" => "frolic",
+    "07" => "gregorian",
+    "08" => "history",
+    "09" => "imbecile",
+    "10" => "jingo",
+    "11" => "kindergarten",
+    "12" => "lambast",
+    "13" => "microphone",
+    "14" => "nativity",
+    "15" => "ostracize",
+    "16" => "preliminary",
+    "17" => "quotidian",
+    "18" => "rastafarian",
+    "19" => "solstice",
+    "20" => "tranquil",
+    "21" => "umbrage",
+    "22" => "victorian",
+    "23" => "waxen",
+    "24" => "xanadu",
+    "25" => "yesteryear",
+    "26" => "zither"
+];
+
+?>
